39e27efd87ade1adb02a205a52f001a1d243c22a
Nikolay Krupiy <hnkovr@gmail.com>
2019-09-03 12:31:21 +0300

    ver 1.0 release

--- num_to_excelCode.py
+++ num_to_excelCode.py
@@ -1,42 +1,65 @@
-_dbg_=True
-_dbg_=False
+#python 3.7 (f-strings're using)
+# This code online-demo: https://repl.it/repls/FriendlyEnlightenedSpools
 
-def prt(*s): print(s)
-def dbg(*s):
-    if _dbg_: prt(s)
-def dbg_var(**vars): dbg(vars)
-def _dbg(var, val): prt(f'{var} =', val)
-def err(n: int): raise ValueError(f"Bad value for n = {n}")
+N_LETTERS = 26
 
-def int2char(n: int) -> str:
-    if n < -1: err(n)
-    if n < 0: return ""
-    return chr(ord('A') + n)
 
-def num_to_excelCode(n: int) -> str:
-    x = 26; s = 0
-    r1 = n // x - 1
-    if r1 >= x:
-        l1 = num_to_excelCode(r1)
-        s = 1
-    r2 = n % x
-    dbg_var(n=n, r1=r1, r2=r2)
-    if not s:
-        l1 = int2char(r1)
-    l2 = int2char(r2)
-    r = l1 + l2
-    dbg(r)
-    return r
+def num_to_excel_сode(number):
+    """Getting Excel-style code from number
+    :param number: number for coding
+    :return: srting with Excel-style code
+    """
 
-def print_res(n):
-    print(str(f'num_to_excelCode({n})=' + num_to_excelCode(n)))
+    def get_letter_by_code(n):
+        """Getting letter by number code
+        :param n: number code
+        :return: str (1 symbol)
+        """
+        if n >= -2:
+            return chr(ord('A') + n)
+        else:
+            raise ValueError(f"Bad value for n = {n}")
 
-def demo(FROM, NR):
-    TO = FROM + NR
-    for i in range(FROM, TO):
-        print_res(i)
+    number -= 1  # for starting with "1" (num_to_excel_сode(1)= "A")
+    letter1_code = number // N_LETTERS - 1
+    if letter1_code > N_LETTERS:
+        letter1 = num_to_excel_сode(letter1_code)
+    elif letter1_code == 1:
+        # remove extra symbol "@" for first 26 decodings (the only way I found for the moment))
+        letter1 = ''
+    else:
+        letter1 = get_letter_by_code(letter1_code)
+    letter2_code = number % N_LETTERS
+    letter2 = get_letter_by_code(letter2_code)
+    return letter1 + letter2 \
+        if letter1 != '@' \
+        else letter2  # the only way i've found to fight with extra "@" for 1st 26 letters :-)
 
-demo(0, 3)
-demo(-2 + 26, 3)
-demo(-2 + 26*26, 3)
-demo(-2 + 26*26*26, 3)
+
+def demo_case(from_nr, to_nr):
+    """Demo of num_to_excel_сode(n) function for one case for range of numbers
+    :param from_nr: start of range
+    :param to_nr: end of range
+    """
+    to_nr = from_nr + to_nr
+    for i in range(from_nr, to_nr):
+        print(str(f'num_to_excel_сode({i})= {num_to_excel_сode(i)}'))
+
+
+def power(a, b):
+    """Getting a power b"""
+    if b == 0: return 1
+    for i in range(b - 1):
+        a *= a
+    return a
+
+
+def demo():
+    """Demo for several cases  at the junctions of threshold values"""
+    nr = 4
+    demo_case(1, nr)
+    for i in range(1, 5):
+        demo_case(-1 + power(N_LETTERS, i), nr)
+
+
+demo()
